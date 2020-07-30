8acf9feaed82e98263535b675cfe15a83d2810a5
Yormynnelly Grajales <yormynnelly.grajales@gmail.com>
2020-07-30 08:40:10 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+class Anagram
+  
+  attr_accessor :word 
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def self.match(list)
+    word_letters = @word.split("").sort 
+    
+    list.find_all do |list_word|
+      list_word.split("").sort == word_letters
+    end 
+  end
+  
+end
+
+    
