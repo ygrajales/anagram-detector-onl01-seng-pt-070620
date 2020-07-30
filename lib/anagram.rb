1eed7015c8b31625726911b2f3992b614fb2e10d
Yormynnelly Grajales <yormynnelly.grajales@gmail.com>
2020-07-30 08:40:51 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+class Anagram
+  
+  attr_accessor :word 
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(list)
+    word_letters = @word.split("").sort 
+    
+    list.find_all do |list_word|
+      list_word.split("").sort == word_letters
+    end 
+  end
+  
+end
+
+    
